a061356039520bf2d2c6207f73c29231670b322b
Sara Carlile <sara.elizabeth.wood@gmail.com>
2015-11-11 15:28:09 -0600

    objects fun

---
+++ README.txt
@@ -0,0 +1,2 @@
+# Objects
+Fun with JavaScript Closures
---
+++ objects.js
@@ -0,0 +1,16 @@
+var jar = {};
+jar.round = {};
+jar.round = {contents: "water", 
+             glass: "green",
+             refill: function(){
+             console.log("fill me");
+            }
+            };
+jar.round.refill();
+
+function full(){
+  console.log("i\'m full");
+}
+
+jar.round.pour = full;
+jar.round.pour();
