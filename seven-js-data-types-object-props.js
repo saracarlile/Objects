9d16b27a9cbb71049c2712bcc6e8dec9219c9164
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-05-26 09:43:51 -0500

    seven JS data types exercise

---
+++ seven-js-data-types-object-props.js
@@ -0,0 +1,31 @@
+//Create a JavaScript object that holds properties of the seven different
+//JavaScript data types: Boolean, Null, Undefined, Number, String, Symbol, and Object.
+
+var dataTypeHolder = {
+    boolean: true,
+    nullValue: null,
+    undefinedValue: undefined,
+    numberValue: 54,
+    stringValue: "JavaScript",
+    symbolValue: Symbol("foo"),
+    greenBall: {
+        size: "small", 
+        color: "green"
+    }     
+};
+
+//Iterate over object and rint values to console.
+for (var val in dataTypeHolder){
+   console.log(dataTypeHolder[val]);
+}
+
+//true
+//null
+//undefined
+//54
+//"JavaScript"
+//[object Symbol] { ... }
+//[object Object] {
+//  color: "green",
+//  size: "small"
+//}
